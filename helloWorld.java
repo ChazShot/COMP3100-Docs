a9a477950af149d403caca7e74b2ee68b04cf089
Jeffrey Chien <jeffchien1@hotmail.com>
2022-03-04 01:31:21 +1100

    initial version of helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Jeffrey Chien: Student ID:45156557
+ */
+
+ class helloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
+ }
